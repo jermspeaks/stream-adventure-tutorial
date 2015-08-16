3e9bdd2515446b505816ea3cdd9428a955ea84da
jermspeaks <jermspeaks@gmail.com>
2015-08-16 14:58:46 -0700

    websockets yay

--- program.js
+++ program.js
@@ -1,7 +1,4 @@
-var request = require('request');
+var ws = require('websocket-stream');
+var stream = ws('ws://localhost:8099');
 
-var r = request.post('http://localhost:8099');
-
-process.stdin
-  .pipe(r)
-  .pipe(process.stdout);
+stream.write('hello\n');
