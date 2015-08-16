339e1b09fd7bf774b4744b592128f096d53c45f9
jermspeaks <jermspeaks@gmail.com>
2015-08-16 14:33:55 -0700

    Program 6 complete

--- program.js
+++ program.js
@@ -1,17 +1,15 @@
-var through2 = require('through2'),
-  split = require('split'),
-  lineCounter = 0;
+var concat = require('concat-stream');
+
+function reverse(s) {
+  return s.split('').reverse().join('');
+}
+
+var concatStream = concat(function(stringBuffer) {
+  console.log(reverse(stringBuffer.toString()));
+  // return reverse(stringBuffer.toString());
+});
 
 process.stdin
-  .pipe(split())
-  .pipe(through2(function (line, _, next) {
-    if (lineCounter % 2 == 1) {
-      console.log(line.toString().toUpperCase());
-    } else {
-      console.log(line.toString().toLowerCase());
-    }
-    lineCounter++;
-    next();
-  }))
-  .pipe(process.stdout)
+  .pipe(concatStream)
+  // .pipe(process.stdout)
 ;
