a1d3006fa298e954d9c528e1b12d255c711af97b
jermspeaks <jermspeaks@gmail.com>
2015-08-16 14:15:55 -0700

    lines split

--- program.js
+++ program.js
@@ -1,13 +1,17 @@
-var through = require('through2'),
-  stream = through(write, end);
+var through2 = require('through2'),
+  split = require('split'),
+  lineCounter = 0;
 
-function write(buffer, encoding, next) {
-  this.push(buffer.toString().toUpperCase());
-  next();
-}
-
-function end(done) {
-  done();
-}
-
-process.stdin.pipe(stream).pipe(process.stdout);
+process.stdin
+  .pipe(split())
+  .pipe(through2(function (line, _, next) {
+    if (lineCounter % 2 == 1) {
+      console.log(line.toString().toUpperCase());
+    } else {
+      console.log(line.toString().toLowerCase());
+    }
+    lineCounter++;
+    next();
+  }))
+  .pipe(process.stdout)
+;
