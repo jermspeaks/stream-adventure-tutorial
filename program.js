6716cf4000bf5944c10de16cf1e5029e173b2c9e
jermspeaks <jermspeaks@gmail.com>
2015-08-16 21:06:12 -0700

    Counting Countries

--- program.js
+++ program.js
@@ -1,7 +1,19 @@
-var spawn = require('child_process').spawn,
-  duplexer = require('duplexer');
+var duplexer = require('duplexer2');
+var through = require('through2').obj;
 
-module.exports = function(cmd, args) {
-  var stream = spawn(cmd, args);
-  return duplexer(stream.stdin, stream.stdout);
-}
+module.exports = function(counter) {
+  var countObj = {};
+  var input = through(write, end);
+
+  function write(row, _, next) {
+    countObj[row.country] = (countObj[row.country] || 0) + 1;
+    next();
+  }
+
+  function end(done) {
+    counter.setCounts(countObj);
+    done();
+  }
+
+  return duplexer(input, counter);
+};
