e91f951fbf4f4182256d9da5a1e72f61e8c783fb
jermspeaks <jermspeaks@gmail.com>
2015-08-16 21:20:16 -0700

    decrypt all your things

--- program.js
+++ program.js
@@ -1,35 +1,7 @@
-var combine = require('stream-combiner');
-var split = require('split');
-var zlib = require('zlib');
-var through = require('through2');
+var crypto = require('crypto');
 
-module.exports = function(counter) {
-  var grouper = through(write, end);
-  var current;
+var stream = crypto.createDecipher('aes256', process.argv[2]);
 
-  function write(line, _, next) {
-    if (line.length === 0) {
-      return next();
-    }
-    var row = JSON.parse(line);
-
-    if (row.type === 'genre') {
-      if (current) {
-        this.push(JSON.stringify(current) + '\n');
-      }
-      current = { name: row.name, books: [] }
-    } else if (row.type === 'book') {
-      current.books.push(row.name);
-    }
-    next();
-  }
-
-  function end(next) {
-    if (current) {
-      this.push(JSON.stringify(current) + '\n');
-    }
-    next();
-  }
-
-  return combine(split(), grouper, zlib.createGzip());
-};
+process.stdin
+  .pipe(stream)
+  .pipe(process.stdout);
