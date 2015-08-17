7257c0faa1c484cf4336697dc5cdc296cf146837
jermspeaks <jermspeaks@gmail.com>
2015-08-16 21:28:07 -0700

    final secretz

--- program.js
+++ program.js
@@ -1,7 +1,28 @@
+var concat = require('concat-stream');
 var crypto = require('crypto');
+var zlib = require('zlib');
+var tar = require('tar')
 
-var stream = crypto.createDecipher('aes256', process.argv[2]);
+
+var cipherName = process.argv[2];
+var passphrase = process.argv[3];
+
+var parser = tar.Parse();
+parser.on('entry', function(e) {
+    if (e.type !== 'File') return;
+
+    var cryptHash = crypto.createHash('md5', {
+        encoding: 'hex'
+    });
+
+    e.pipe(cryptHash).pipe(concat(function(hash) {
+      console.log(hash + ' ' + e.path);
+    }));
+});
 
 process.stdin
-  .pipe(stream)
-  .pipe(process.stdout);
+  .pipe(crypto.createDecipher(cipherName, passphrase))
+  .pipe(zlib.createGunzip())
+  .pipe(parser)
+;
+
